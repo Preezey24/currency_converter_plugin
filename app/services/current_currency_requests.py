edc9af9d7b69c93c80df1fb07c28496e0f22b65e
Rhys Previte <rhys.previte@2hatsecurity.com>
2023-04-16 16:58:07 -0700

    add currency request logic

---
+++ app/services/current_currency_requests.py
@@ -0,0 +1,11 @@
+import requests
+
+def fetch_exchange_rates(base_currency, api_key):
+    try:
+        response = response = requests.get(f'https://api.exchangeratesapi.io/latest?base={base_currency}&access_key={api_key}')
+        response.raise_for_status()
+        data = response.json()
+        return data['rates']
+    except requests.exceptions.RequestException as e:
+        print(f'Error fetching exchange rates: {e}')
+        return None
